eb8bb88829f298b74eefb72ce494877250bbc5a7
RithmaRandima <rithma.r.nandasiri@gmail.com>
2024-07-28 01:18:40 +0530

    Add React Slick to Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,30 @@ import imgRightBig from "../../assets/Hero-Right-Big.jpg";
 import imgRightTop from "../../assets/Hero-Right-Top.jpg";
 import imgRightBottomSmall from "../../assets/Hero-Right-Small.jpg.jpg";
 
+import image1 from "../../assets/Hero-Left-Top.jpg";
+import image2 from "../../assets/Hero-Right-Big.jpg";
+import image3 from "../../assets/Hero-Right-Top.jpg";
+import image4 from "../../assets/News-Letter-Top.jpg";
+import image5 from "../../assets/about-04.jpg";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const sideImages = [image1, image2, image3, image4, image5];
+
 const Hero = () => {
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    cssEase: "linear",
+  };
+
   return (
     <div className="relative w-[100%] h-[120vh] md:h-[100vh] bg-white">
       {/* center text */}
@@ -26,7 +49,19 @@ const Hero = () => {
       </div>
 
       {/* big left img & big top */}
-      <div className="hero-big-left-image absolute  top-0 w-full h-[180px]  md:top-[50px] md:left-[20px] md:w-[300px] md:h-[500px] flex overflow-hidden"></div>
+      <div className="absolute top-0 w-full h-[180px]  md:top-[50px] md:left-[20px] md:w-[300px] md:h-[500px]">
+        <Slider {...settings}>
+          {sideImages.map((images) => {
+            return (
+              <img
+                src={images}
+                className="w-[100%] h-[480px] object-cover"
+                alt=""
+              />
+            );
+          })}
+        </Slider>
+      </div>
 
       {/* left img 1 & big top */}
       <div className="hidden md:block absolute top-[80px] left-[250px] h-[250px] w-[200px]">
@@ -46,7 +81,19 @@ const Hero = () => {
       </div>
 
       {/* big right img big bottom img */}
-      <div className="hero-big-right-image absolute block bottom-0 w-full h-[180px] md:top-[30px] md:right-[100px] md:w-[310px] md:h-[400px]"></div>
+      <div className="hero-big-right-image absolute block bottom-0 w-full h-[180px] md:top-[30px] md:right-[60px] md:w-[360px] md:h-[400px]">
+        <Slider {...settings}>
+          {sideImages.map((images) => {
+            return (
+              <img
+                src={images}
+                className="w-[100%] h-[480px] object-cover"
+                alt=""
+              />
+            );
+          })}
+        </Slider>
+      </div>
 
       {/* right img 1 & big top */}
       <div className="hidden md:block absolute  top-[190px] right-[20px] h-[350px] w-[230px]">
